51f3dd474d5c4ea65faabe2fdc30516e631f867e
Devat22 <dev.ac.da90@gmail.com>
2019-08-27 15:04:10 -0500

    upload

---
+++ app/src/main/java/com/rjt/loginfrags/activities/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.rjt.loginfrags.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rjt.loginfrags.R
+import com.rjt.loginfrags.fragments.HomeFragment
+import com.rjt.loginfrags.fragments.LoginFragment
+import com.rjt.loginfrags.helpers.SessionManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+    fun init(){
+        val session:SessionManager = SessionManager(this)
+        if(session.isLoggedIn()){
+            val fr = HomeFragment()
+            val bundle = Bundle()
+            bundle.putString("name", session.sharedPreference.getString("name",""))
+            val tr = supportFragmentManager.beginTransaction()
+            tr.replace(R.id.frc, fr)
+            tr.commit()
+        }else{
+            val fr = LoginFragment()
+            val tr = supportFragmentManager.beginTransaction()
+            tr.replace(R.id.frc, fr)
+            tr.commit()
+        }
+    }
+}
